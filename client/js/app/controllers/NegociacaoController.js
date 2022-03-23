e53fc4e53bead17ca2c9db17b7abe580db954caa
Bruno Rocha de Lima <brunorocha33@live.com>
2022-03-23 14:06:50 -0300

    Tratando objeto data

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -1,18 +1,33 @@
 class NegociacaoController {
-  
+  #inputData;
+  #inputQuantidade;
+  #inputValor;
+
   constructor() {
-    let $ = document.querySelector.bind(document)
-    
-    this.inputData = $('#data')
-    this.inputQuantidade = $('#quantidade')
-    this.inputValor = $('#valor')
-  }
-  
-  adiciona(event) {
-    event.preventDefault();
-    console.log(this.inputData.value)
-    console.log(this.inputQuantidade.value)
-    console.log(this.inputValor.value)
+    let $ = document.querySelector.bind(document); // bind indica que o valor the this será document
+
+    this.#inputData = $("#data");
+    this.#inputQuantidade = $("#quantidade");
+    this.#inputValor = $("#valor");
   }
 
+  adiciona(event) {
+    event.preventDefault();
+    let data = new Date(
+      ...this.#inputData.value.split("-").map((item, indice) => {
+        if (indice == 1) {
+          return item - 1;
+        }
+        return item;
+      })
+    );
+
+    let negociacao = new Negociacao(
+      data,
+      this.#inputQuantidade.value,
+      this.#inputValor.value
+    );
+
+    console.log(negociacao);
+  }
 }
